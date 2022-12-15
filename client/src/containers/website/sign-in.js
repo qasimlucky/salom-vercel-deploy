ae2093c24def24f69e95b977fd65c64a2efcb3fd
Qasim Ali <54708575+qasimlucky@users.noreply.github.com>
2022-12-15 14:27:13 +0500

    first commit

--- client/src/containers/website/sign-in.js
+++ client/src/containers/website/sign-in.js
@@ -7,61 +7,66 @@ function SignIn() {
          return (
           <>
            <Header/>
+   {/* stv-signin SECTION STARTS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */}
 
-<div id="app" style={{marginTop:"7px"}} >
-    <section class="section ">
-        <div class="sign-img"  style={{backgroundImage:`url("https://lifeatthemovies.com/wp-content/uploads/2017/09/2000-672x372.png")`,boxShadow: "inset 0rem 0rem 27rem 20rem #1d1d41"}}>
+  <div class="stv-signin" id="app">
+    <section class="section">
+      <div class="sign-img"  style={{backgroundImage:`url("https://lifeatthemovies.com/wp-content/uploads/2017/09/2000-672x372.png")`}}>
         <div class="container" >
-        <div class="row" style={{marginTop : "150px" }}>
-          <div >
-            <div class="card card-primary web-bg" >
-              <h3 class="form-group col-12"  style={{textAlign:"center", marginTop:"20px", color:"white",fontSize:"x-large",fontFamily:"sans-serif"}} >Sign In</h3>
-              
-              <div class="card-body">
-                <form>
-                  <div class="row">
-                    <div class="form-group col-12">
-                      <input id="phone_number" type="text"  class="form-control" name="phone_number"  placeholder="Email or Phone number" style={{backgroundColor: "#1d1d41"}}/>
-                    </div>
-                    
-                  </div> 
-                  <div class="row">
-                  <div class="form-group col-12">
-                        <div class="input-group-prepend" >
-                        <input  id="password" type="password" name="password"   class="form-control"  placeholder="Password"  style={{ backgroundColor: "#1d1d41"}}/>
-                      </div>
-                      
-                      <p style={{color:"red",textAlign:"right",fontSize:"larger"}}>Forgot your password</p>
-                      
-                    </div>
-                  </div>
+          <div class="row signin-box">
+            <div class="signin-box-inner">
+              <div class="card card-primary web-bg" >
+                <h3 class="form-group col-12 signin-h3">
+                  Sign In
+                </h3>
+                
+                <div class="card-body">
+                  <form>
 
-                  <div class="row">
-                  {/* <div class="form-group col-2"></div> */}
-                    <div class="form-group col-12" style={{textAlign:"center",borderRadius:"20px"}}>
-                     <button  type="submit" class="btn btn-lg btn-block" style={{borderRadius:"20px",backgroundColor:"#d51c58", color:"whitesmoke",fontSize:"large"}}>
+                    <div class="row">
+                      <div class="form-group col-12">
+                        <input id="phone_number" type="text"  class="form-control input" name="phone_number"  placeholder="Email or Phone number"/>
+                      </div>
+                    </div> 
+
+                    <div class="row">
+                      <div class="form-group col-12">
+                        <div class="input-group-prepend" >
+                          <input  id="password" type="password" name="password"   class="form-control input"  placeholder="Password"/>
+                        </div>
+                        <p class="signin-forgot">Forgot your password</p>
+                      </div>
+                    </div>
+
+                    <div class="row">
+                      {/* <div class="form-group col-2"></div> */}
+                      <div class="form-group col-12 signin-btn-box">
+                        <button  type="submit" class="btn btn-lg btn-block">
                           Sign In
                         </button>   
-                        </div>
-                        
-                        <div class="form-group col-2">
-                        </div>
-                  </div>
-                  <div style={{paddingBottom:"10%"}}>
-                  <input type="checkbox" value="isRememberMe" id="rememberMe"></input> <label style={{color:"white",fontSize:"larger"}}>Remember me</label>
-                  </div>
-                  <div >
-                    <p style={{color:"white", lineHeight:"11px", textAlign:"center",fontSize:"larger"}}>Don't have an account? <a style={{color:"red"}}> Sign up</a><br></br> </p>
-                  </div>
-                </form>
+                      </div>
+                      <div class="form-group col-2"></div>
+                    </div>
+
+                    <div class="signin-rem">
+                      <input type="checkbox" value="isRememberMe" id="rememberMe"></input> 
+                      <label>Remember me</label>
+                    </div>
+
+                    <div class="signin-signup">
+                      <p>
+                        Don't have an account? 
+                        <a> Sign up</a><br></br> 
+                      </p>
+                    </div>
+
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-        </div>
-      
     </section>
   </div>
   <WebFooter/>  
--- client/src/containers/website/sign-up.js
+++ client/src/containers/website/sign-up.js
@@ -7,17 +7,17 @@ function SignUp() {
           <>
            <Header/>
 
-<div id="app" style={{marginTop:"7px"}} >
+<div class="stv-signup" id="app">
     <section class="section ">
-        <div class="sign-img"  style={{backgroundImage:`url("https://lifeatthemovies.com/wp-content/uploads/2017/09/2000-672x372.png")`,boxShadow: "inset 0rem 0rem 27rem 20rem #1d1d41"}}>
-        <div class="container" style={{height:"100%"}} >
-        <div class="row" style={{height:"100%",margin:"auto" }}>
-          <div style={{margin:"auto" }}>
+        <div class="sign-img"  style={{backgroundImage:`url("https://lifeatthemovies.com/wp-content/uploads/2017/09/2000-672x372.png")`}}>
+        <div class="container">
+        <div class="row signup-box">
+          <div class="signup-box-inner">
             <div class="card card-primary web-bg" >
-              <h3 class="form-group col-12"  style={{textAlign:"center", marginTop:"20px", color:"white",fontSize:"x-large",fontFamily:"sans-serif"}} >Sign Up</h3>
+              <h3 class="form-group col-12 signup-h3">Sign Up</h3>
               
               <div class="card-body">
-                <form style={{fontFamily:"sans-serif"}}>
+                <form>
                   {/* <div class="row">
                     <div class="form-group col-12" >
                       <input id="fullName" type="text"  class="form-control" name="full_name"  placeholder="Full Name"  style={{backgroundColor: "#1d1d41", border:"#1d1d41"}} />
@@ -33,9 +33,8 @@ function SignUp() {
                   </div> */}
                   <div class="row">
                     <div class="form-group col-12">
-                    <input type="text" class="form-control" placeholder="D.O.B" style={{backgroundColor: "#1d1d41",color:"grey", border:"#1d1d41"}}/>
+                      <input type="text" class="form-control input" placeholder="D.O.B"/>
                     </div>
-                    
                   </div>
                   {/* <div class="row">
                     <div class="form-group col-12">
@@ -45,35 +44,35 @@ function SignUp() {
                   </div>  */}
                   <div class="row">
                     <div class="form-group col-12">
-                        <select name="choice" class="form-control" style={{backgroundColor: "#1d1d41",color:"grey",height:"42px", border:"#1d1d41"}}>
-                        <option value="first" style={{backgroundColor: "#1d1d41",color:"grey"}}>Male</option>
-                        <option value="second" style={{backgroundColor: "#1d1d41",color:"grey"}} selected>Gender</option>
-                        <option value="third" style={{backgroundColor: "#1d1d41",color:"grey"}}>Female</option>
-                        </select>
+                      <select name="choice" class="form-control select-gender">
+                        <option class="option-gender" value="first">Male</option>
+                        <option class="option-gender" value="second" selected>Gender</option>
+                        <option class="option-gender" value="third">Female</option>
+                      </select>
+                    </div>
                   </div>
-                  
-                  </div>
+
                   <div class="row">
                     <div class="form-group col-12">
-                        <select name="choice" class="form-control" style={{backgroundColor: "#1d1d41",color:"grey",height:"42px", border:"#1d1d41"}}>
-                        <option value="first" style={{backgroundColor: "#1d1d41",color:"grey"}}>Pakistan</option>
-                        <option value="second" style={{backgroundColor: "#1d1d41",color:"grey"}} selected>Country</option>
-                        <option value="third" style={{backgroundColor: "#1d1d41",color:"grey"}}>India</option>
+                        <select name="choice" class="form-control select-gender">
+                        <option class="option-gender" value="first">Pakistan</option>
+                        <option class="option-gender" value="second" selected>Country</option>
+                        <option class="option-gender" value="third">India</option>
                         </select>
                     </div>
                   
                   </div>
                   <div class="row">
                     <div class="form-group col-12">
-                        <input  id="password" type="password" name="password"   class="form-control" data-indicator="pwindicator" placeholder="Password"  style={{backgroundColor: "#1d1d41", border:"#1d1d41",color:"grey"}}/>
+                        <input  id="password" type="password" name="password"   class="form-control input" data-indicator="pwindicator" placeholder="Password"/>
                       
                     </div>
                   </div>
 
                   <div class="row">
                   {/* <div class="form-group col-2"></div> */}
-                    <div class="form-group col-12">
-                     <button  type="submit" class="btn btn-lg btn-block" style={{borderRadius:"20px",backgroundColor:"#d51c58",color:"whitesmoke",fontSize:"large"}}>
+                    <div class="form-group col-12 signup-btn-box">
+                     <button  type="submit" class="btn btn-lg btn-block">
                           Singn Up
                         </button>   
                         </div>
@@ -81,11 +80,16 @@ function SignUp() {
                         <div class="form-group col-2">
                         </div>
                   </div>
-                  <div>
-                  <input type="checkbox"/> <label style={{color:"white",fontSize:"larger"}}>I accept<a style={{color:"red"}}> Terms and conditions</a></label>
+                  <div class="signup-accept">
+                    <input type="checkbox"/>
+                    <label>I accept
+                      <a> Terms and conditions</a>
+                    </label>
                   </div>
                   <div >
-                    <p style={{color:"white", lineHeight:"40px", textAlign:"center",fontSize:"larger"}}>Alread have an account? <a style={{color:"red"}}> Sign In</a></p>
+                    <p class="already-account">Alread have an account? 
+                      <a> Sign In</a>
+                    </p>
                   </div>
                 </form>
               </div>
